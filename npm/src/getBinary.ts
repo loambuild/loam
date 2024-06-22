d34c7b1cc406f05e3b4f68d5f0fd627803b7ecc3
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2024-06-21 21:21:04 -0400

    Fix/npm package (#86)

--- npm/src/getBinary.ts
+++ npm/src/getBinary.ts
@@ -1,28 +1,28 @@
-import { Binary } from ".";
-import { join } from "path";
 import * as os from "os";
-const {version} = require("../package.json");
+import { join } from "path";
+import { Binary } from ".";
+const { version } = require("../package.json");
 
-const NAME = "loam"
+const NAME = "loam";
 
 function getPlatform() {
   const type = os.type();
   const arch = os.arch();
   let typeDict = {
-    "Darwin": "apple-darwin",
-    "Linux": "unknown-linux-gnu",
-    "Windows_NT": "pc-windows-msvc"
+    Darwin: "apple-darwin",
+    Linux: "unknown-linux-gnu",
+    Windows_NT: "pc-windows-msvc",
   };
 
   let archDict = {
-    "x64": "x86_64",
-    "arm64": "aarch64"
+    x64: "x86_64",
+    arm64: "aarch64",
   };
 
-  //@ts-ignore 
- let rust_type: string? = typeDict[type];
- //@ts-ignore 
- let rust_arch: string? = archDict[arch];
+  //@ts-ignore
+  let rust_type: string? = typeDict[type];
+  //@ts-ignore
+  let rust_arch: string? = archDict[arch];
 
   if (rust_type && rust_arch) {
     return [rust_type, rust_arch];
@@ -32,16 +32,12 @@ function getPlatform() {
 
 export function GithubUrl(): string {
   const [platform, arch] = getPlatform();
-  return `https://github.com/loambuild/loam-sdk/releases/download/v${version}/${NAME}-v${version}-${arch}-${platform}.tar.gz`;
+  return `https://github.com/loambuild/loam-sdk/releases/download/loam-cli-v${version}/loam-cli-v${version}-${arch}-${platform}.tar.gz`;
 }
 
 export function getBinary(name: string = NAME): Promise<Binary> {
   if (!process.env["LOAM_BIN_PATH"]) {
-    process.env["LOAM_BINARY_PATH"] = join(
-      os.homedir(),
-      `.${NAME}`,
-      NAME
-    );
+    process.env["LOAM_BINARY_PATH"] = join(os.homedir(), `.${NAME}`, NAME);
   }
 
   // Will use version after publishing to AWS
