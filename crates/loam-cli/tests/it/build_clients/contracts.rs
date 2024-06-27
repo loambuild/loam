8ebebe2946be95d9b4136737d7a44498f63364c8
Blaine Heffron <baheffron@gmail.com>
2024-06-27 19:41:29 -0400

    fmt

--- crates/loam-cli/tests/it/build_clients/contracts.rs
+++ crates/loam-cli/tests/it/build_clients/contracts.rs
@@ -96,29 +96,41 @@ network-passphrase = "Standalone Network ; February 2017"
 hello_world.workspace = true
 "#,
         );
-        let output = env.loam_env("build", "development").output().expect("Failed to execute command");
+        let output = env
+            .loam_env("build", "development")
+            .output()
+            .expect("Failed to execute command");
 
         // ensure it imports
         assert!(output.status.success());
-        assert!(String::from_utf8_lossy(&output.stderr).contains("🍽️ importing \"hello_world\" contract"));
+        assert!(String::from_utf8_lossy(&output.stderr)
+            .contains("🍽️ importing \"hello_world\" contract"));
 
-        let output2 = env.loam_env("build", "development").output().expect("Failed to execute command");
+        let output2 = env
+            .loam_env("build", "development")
+            .output()
+            .expect("Failed to execute command");
 
         // ensure alias retrieval works
         assert!(output2.status.success());
-        assert!(String::from_utf8_lossy(&output2.stderr).contains("✅ Contract \"hello_world\" is up to date"));
+        assert!(String::from_utf8_lossy(&output2.stderr)
+            .contains("✅ Contract \"hello_world\" is up to date"));
 
-        let file= "contracts/increment/src/lib.rs";
-        let file_replaced= "contracts/hello_world/src/lib.rs";
+        let file = "contracts/increment/src/lib.rs";
+        let file_replaced = "contracts/hello_world/src/lib.rs";
         env.replace_file(file, file_replaced);
 
-        let output3 = env.loam_env("build", "development").output().expect("Failed to execute command");
+        let output3 = env
+            .loam_env("build", "development")
+            .output()
+            .expect("Failed to execute command");
 
         // ensure contract hash change check works, should update in dev mode
         assert!(output3.status.success());
-        assert!(String::from_utf8_lossy(&output3.stderr).contains("🔄 Updating contract \"hello_world\""));
+        assert!(String::from_utf8_lossy(&output3.stderr)
+            .contains("🔄 Updating contract \"hello_world\""));
 
-       env.set_environments_toml(
+        env.set_environments_toml(
             r#"
 production.accounts = [
     { name = "alice" },
@@ -134,11 +146,14 @@ hello_world.workspace = true
         );
         env.replace_file("contracts/auth/src/lib.rs", file_replaced);
 
-        let output4 = env.loam("build").output().expect("Failed to execute command");
+        let output4 = env
+            .loam("build")
+            .output()
+            .expect("Failed to execute command");
 
         // ensure contract hash change check works, should throw error in production
         assert!(!output4.status.success());
-        assert!(String::from_utf8_lossy(&output4.stderr).contains("⛔ ️Contract update not allowed in production for \"hello_world\""));
+        assert!(String::from_utf8_lossy(&output4.stderr)
+            .contains("⛔ ️Contract update not allowed in production for \"hello_world\""));
     });
 }
-
