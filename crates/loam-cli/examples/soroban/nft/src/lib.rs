b18d6d6fcb106b563e80db7934182ad427579777
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2024-07-15 11:46:53 -0400

    Use derive_contract for example nft

--- crates/loam-cli/examples/soroban/nft/src/lib.rs
+++ crates/loam-cli/examples/soroban/nft/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-use loam_sdk::soroban_contract;
+use loam_sdk::derive_contract;
 use loam_subcontract_core::{admin::Admin, Core};
 
 pub mod nft;
@@ -8,18 +8,9 @@ pub mod subcontract;
 use nft::MyNonFungibleToken;
 use subcontract::{Initable, NonFungible};
 
+#[derive_contract(
+    Core(Admin),
+    NonFungible(MyNonFungibleToken),
+    Initable(MyNonFungibleToken)
+)]
 pub struct Contract;
-
-impl Core for Contract {
-    type Impl = Admin;
-}
-
-impl NonFungible for Contract {
-    type Impl = MyNonFungibleToken;
-}
-
-impl Initable for Contract {
-    type Impl = MyNonFungibleToken;
-}
-
-soroban_contract!();
