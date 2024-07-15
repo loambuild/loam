95d21610ed5879134a9a73bb72e584f9615d3327
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2024-07-15 14:49:44 -0400

    fix: use doc comments for subcontracts
    Now when I run this:
    
    ```
    just build && \
        stellar contract deploy --wasm target/loam/example_nft.wasm --source alice --network local --alias nft && \
        stellar contract invoke --id nft -- --help
    ```
    
    I get this output:
    
    ```
    Commands:
      admin_get                Get current admin
      admin_set                Transfer to new admin
                                   Should be called in the same transaction as deploying the contract to ensure that
                                   a different account try to become admin
      redeploy                 Admin can redepoly the contract with given hash.
      mint                     Mint a new NFT with the given ID, owner, and metadata
      transfer                 Transfer an NFT with the given ID from current_owner to new_owner
      get_nft                  Get the NFT with the given ID
      get_owner                Find the owner of the NFT with the given ID
      get_total_count          Get the total count of NFTs
      get_collection_by_owner  Get all of the NFTs owned by the given address
      nft_init                 Initialize the NFT contract with the given admin and name
    ```

--- crates/loam-cli/examples/soroban/nft/src/subcontract.rs
+++ crates/loam-cli/examples/soroban/nft/src/subcontract.rs
@@ -2,14 +2,14 @@ use loam_sdk::{soroban_sdk::Lazy, subcontract};
 
 #[subcontract]
 pub trait IsNonFungible {
-    // Mint a new NFT with the given ID, owner, and metadata
+    /// Mint a new NFT with the given ID, owner, and metadata
     fn mint(
         &mut self,
         owner: loam_sdk::soroban_sdk::Address,
         metadata: loam_sdk::soroban_sdk::Bytes,
     ) -> u32;
 
-    // Transfer an NFT with the given ID from current_owner to new_owner
+    /// Transfer an NFT with the given ID from current_owner to new_owner
     fn transfer(
         &mut self,
         id: u32,
@@ -17,16 +17,16 @@ pub trait IsNonFungible {
         new_owner: loam_sdk::soroban_sdk::Address,
     );
 
-    // Get the NFT with the given ID
+    /// Get the NFT with the given ID
     fn get_nft(&self, id: u32) -> Option<loam_sdk::soroban_sdk::Bytes>;
 
-    // Find the owner of the NFT with the given ID
+    /// Find the owner of the NFT with the given ID
     fn get_owner(&self, id: u32) -> Option<loam_sdk::soroban_sdk::Address>;
 
-    // Get the total count of NFTs
+    /// Get the total count of NFTs
     fn get_total_count(&self) -> u32;
 
-    // Get all of the NFTs owned by the given address
+    /// Get all of the NFTs owned by the given address
     fn get_collection_by_owner(
         &self,
         owner: loam_sdk::soroban_sdk::Address,
@@ -35,5 +35,6 @@ pub trait IsNonFungible {
 
 #[subcontract]
 pub trait IsInitable {
+    /// Initialize the NFT contract with the given admin and name
     fn nft_init(&self, admin: loam_sdk::soroban_sdk::Address, name: loam_sdk::soroban_sdk::Bytes);
 }
