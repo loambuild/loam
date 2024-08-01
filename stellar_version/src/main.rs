3a5c5f5e751c14d6f3303835e590a1991183ec42
Blaine Heffron <baheffron@gmail.com>
2024-08-01 17:36:13 -0400

    fmt

--- stellar_version/src/main.rs
+++ stellar_version/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let metadata = match MetadataCommand::new().exec() {
         Ok(metadata) => metadata,
         Err(error) => {
-            eprintln!("Failed to get cargo metadata: {}", error);
+            eprintln!("Failed to get cargo metadata: {error}");
             process::exit(1);
         }
     };
@@ -15,7 +15,7 @@ fn main() {
     for package in metadata.packages {
         if package_is_using_soroban_cli(&package) {
             if let Some(version) = get_soroban_cli_version(&package) {
-                println!("{}", version);
+                println!("{version}");
                 return;
             }
         }
@@ -26,11 +26,16 @@ fn main() {
 }
 
 fn package_is_using_soroban_cli(package: &Package) -> bool {
-    package.dependencies.iter().any(|dep| dep.name == "soroban-cli")
+    package
+        .dependencies
+        .iter()
+        .any(|dep| dep.name == "soroban-cli")
 }
 
 fn get_soroban_cli_version(package: &Package) -> Option<String> {
-    package.dependencies.iter()
-                        .find(|dep| dep.name == "soroban-cli")
-                        .map(|dep| dep.req.to_string())
+    package
+        .dependencies
+        .iter()
+        .find(|dep| dep.name == "soroban-cli")
+        .map(|dep| dep.req.to_string())
 }
